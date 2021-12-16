976a0f9c0e168e44fb8630df03573416452e5099
romain callewaert <84373640+zuwui@users.noreply.github.com>
2021-12-16 20:42:33 +0100

    change

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +22,24 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'name' => 'Créer un article',
         ]);
+    }
+
+    /**
+     * @Route("/article", name="article")
+     */
+    public function new(Request $request): Response 
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                    ->add('name')
+                    ->add('slug')
+                    ->add('content')
+                    ->getForm();
+        
+        return $this->render('article/index.html.twig', [
+            'formArticle' => $form
+        ]);       
+        
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+    /**
+     * @var ContactRepository
+     */
+
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository){
+        $this->contactRepository = $contactRepository;
+    }
+
     /**
      * @Route("/contact", name="contact")
      */
@@ -16,6 +27,7 @@ class ContactController extends AbstractController
         // $name = $request->get('name');
         return $this->render('contact/index.html.twig', [
             'name' => 'Romain',
+            'contacts' => $this->contactRepository->findAll()
         ]);
     }
 
@@ -27,6 +39,18 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'name' => 'Romain',
             'city' => $city,
+        ]);
+    }
+
+    /**
+     * @Route("/contact/{id}", name="contactID")
+     */
+    public function id(string $id): Response
+    {
+        $id = $this->contactRepository->find($id);
+        
+        return $this->render('contact/index.html.twig', [
+            'id' => $id,                                 
         ]);
     }
 }
